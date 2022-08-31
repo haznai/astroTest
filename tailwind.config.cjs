4fbc77269f6ebcd674924e9a5e653c4e09a4bbbb
hazn <aitakprojects@gmail.com>
2022-08-31 12:05:21 +0200

    update fonts from `pro` to up-to-date versions

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,9 +3,9 @@ module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     fontFamily: {
-      sans: "Source Sans Pro, ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Helvetica Neue, Arial, Noto Sans, sans-serif, Apple Color Emoji, Segoe UI Emoji, Segoe UI Symbol, Noto Color Emoji",
+      sans: "Source\\ Sans\\ 3, ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Helvetica Neue, Arial, Noto Sans, sans-serif, Apple Color Emoji, Segoe UI Emoji, Segoe UI Symbol, Noto Color Emoji",
       serif:
-        'Source Serif Pro, ui-serif, Georgia, Cambria, "Times New Roman", Times, serif',
+        'Source\\ Serif\\ 4, ui-serif, Georgia, Cambria, "Times New Roman", Times, serif',
     },
     extend: {},
   },
